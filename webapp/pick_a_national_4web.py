0de8d9a4c95d6eadad7eea141e4a08edaca98b73
YouYou-Chen <1908863116@qq.com>
2017-06-21 10:46:06 +0800

    x

--- webapp/pick_a_national_4web.py
+++ webapp/pick_a_national_4web.py
@@ -19,15 +19,15 @@ def entry_page() -> 'html':
                            the_list_items = c_list ,
                            the_title='欢迎来到网上选国！')
 
-@app.route('/pick_a_national', methods=['POST'])
+@app.route('/pick_a_country', methods=['POST'])
 def pick_a_color() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    user_country_name = request.form['user_country']	
-    user_country_code = c_dict_reverse[user_country_name]	
+    user_national_name = request.form['user_national']	
+    user_national_code = c_dict_reverse[user_national_name]	
     return render_template('results.html',
                            the_title = '以下是您选取的国：',
-                           the_country_code = user_country_code,
-                           the_country_name = user_country_name,
+                           the_national_code = user_national_code,
+                           the_national_name = user_national_name,
                            )
 
 if __name__ == '__main__':
