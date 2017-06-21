bd57d0038184d5aea3f0ca819937255941551933
YouYou-Chen <1908863116@qq.com>
2017-06-21 10:02:52 +0800

    z

--- webapp/country.py
+++ webapp/country.py
@@ -4,7 +4,7 @@
 """创建一个类"""
 class country_list_name (object):
 
-    def __init__(self, fn='data\country.tsv'):
+    def __init__(self, fn='data\national.tsv'):
        import csv
        with open(fn, 'r', encoding='utf8') as csvfile:
            reader = csv.DictReader(csvfile, fieldnames=['c_code', 'c_name'], delimiter='\t')
