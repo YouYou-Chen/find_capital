4e494b2fb4c17a73e658bd49b36297d30fcc34f1
YouYou-Chen <1908863116@qq.com>
2017-06-25 15:31:16 +0800

    x

--- webapp/pick_a_zb_national.py
+++ webapp/pick_a_zb_national.py
@@ -5,7 +5,7 @@ import pandas as pd
 df = pd.DataFrame.from_csv("data/fsnd_national.tsv", encoding='utf8', sep='\t')
 df = df.fillna('<i>（缺省值）</i>')   # nan 用  '（缺省值）' 代替
 national = df.set_index('ISO').to_dict()
-print("指标有",len(meta['WEO_Subject_Code']),'个')
+print("指标有",len(national['WEO_Subject_Code']),'个')
 
 
  
